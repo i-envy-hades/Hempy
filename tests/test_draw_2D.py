b5a2712e5b86c825e1d252b1584d393ef81f6629
i-envy-hades <160866344+i-envy-hades@users.noreply.github.com>
2024-05-24 12:01:36 +0200

    finished writing test

--- tests/test_draw_2D.py
+++ tests/test_draw_2D.py
@@ -1,15 +1,4 @@
-from rdkit import Chem
-from rdkit.Chem import AllChem
-from rdkit import DataStructs
-from sklearn.metrics import pairwise_distances
-import numpy as np
-
-from rdkit.Chem import Draw
-from rdkit.Chem import Draw
-#All imports required for the functions, just like the code
-
-from hempy.molecule_visualization import *
-
+from hempy.molecule_visualization import draw_2D
 
 
 
@@ -19,6 +8,6 @@ def test_draw_2D():
 
     molecule_2 = "invalid" #Select an invalid name
     assert draw_2D(molecule_2) == "Impossible de dessiner la molécule, mauvais format pour les Smiles." 
-#Verify that the incorrect smiles raises an error
+#Verify that the incorrect SMILES raises an error
 
 
--- tests/test_draw_3D.py
+++ tests/test_draw_3D.py
@@ -1,6 +1,4 @@
-
-from hempy.molecule_visualization import *
-
+from hempy.molecule_visualization import draw_3D
 
 
 
@@ -8,5 +6,4 @@ def test_draw_3D():
     THC = "CCCCCC1=CC(=C2C3C=C(CCC3C(OC2=C1)(C)C)C)O" #Choose a random SMILES code, for example propane
     draw_3D(THC, "THC.pdb")
 #Verify that something is drawn
-
 
