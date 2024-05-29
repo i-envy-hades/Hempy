122945b45161cd730c03229c0155cb5439493bff
LoumaCer <161627849+LoumaCer@users.noreply.github.com>
2024-05-29 22:25:21 +0200

    finally finished test_3D

--- tests/test_draw_3D.py
+++ tests/test_draw_3D.py
@@ -1,11 +1,24 @@
 from hempy.molecule_visualization import draw_3D
+
+
+import os 
+import nglview
 from rdkit import Chem
 
-def test_draw_3D():
-    THC_smiles = "CCCCCC1=CC(=C2C3C=C(CCC3C(OC2=C1)(C)C)C)O"
+
+
+def test_draw_3D(tmpdir):
+    THC_smiles = "CCCCCC1=CC(=C2C3C=C(CCC3C(OC2=C1)(C)C)C)O"  # Ethanol SMILES string 
     THC_mol = Chem.MolFromSmiles(THC_smiles)
-    filename = "THC.pdb"
-    draw_3D(THC_mol, filename)
+    filename = tmpdir.join("THC.pdb")
+    view = draw_3D(THC_mol, str(filename)) # Check if the file was created
+    assert os.path.exists(str(filename)), "PDB file was not created" 
+    # Verify the view 
+    assert view is not None, "No view returned" 
+    assert isinstance(view, nglview.NGLWidget), "Returned view is not an NGLWidget"
+    # Additional checks could include verifying contents of the file, etc.
+
+
 
 
     
