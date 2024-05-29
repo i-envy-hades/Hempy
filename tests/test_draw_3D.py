00a14acf5683346d01c1a1861cd8d27deb4b5bd2
LoumaCer <161627849+LoumaCer@users.noreply.github.com>
2024-05-29 22:01:02 +0200

    wrote test_3D, works

--- tests/test_draw_3D.py
+++ tests/test_draw_3D.py
@@ -1,9 +1,12 @@
 from hempy.molecule_visualization import draw_3D
-
-
+from rdkit import Chem
 
 def test_draw_3D():
-    stupid = "stupid thing doesn't work"
+    THC_smiles = "CCCCCC1=CC(=C2C3C=C(CCC3C(OC2=C1)(C)C)C)O"
+    THC_mol = Chem.MolFromSmiles(THC_smiles)
+    filename = "THC.pdb"
+    draw_3D(THC_mol, filename)
+
 
     
 
